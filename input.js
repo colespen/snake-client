9f4b0ece9d4699036ccd21207014d7115f07ce4b
colspen <spencerlewiscole@gmail.com>
2022-09-28 19:40:40 +0000

    wasd movement added and communicating with server, praise be to Gary

--- input.js
+++ input.js
@@ -1,23 +1,40 @@
-const { stdin } = require("process");
-
 // setup interface to handle user input from stdin
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  
+
+
   stdin.on("data", handleUserInput);
-  
+ 
   return stdin;
 };
 
-const handleUserInput = function () {
-  stdin.on("data", key => {
+const handleUserInput = function(key) {
+
+    if (key === 'w') {
+      connection.write("Move: up");
+      // console.log('w');
+    }
+    if (key === 'a') {
+      connection.write("Move: left");
+      // console.log('a');
+    }
+    if (key === 's') {
+      connection.write("Move: down");
+      // console.log('s');
+    }
+    if (key === 'd') {
+      connection.write("Move: right");
+      // console.log('d');
+    }
     if (key === '\u0003') {
       process.exit();
     }
-  })
 };
 
 module.exports = setupInput;
