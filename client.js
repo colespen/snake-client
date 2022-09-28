0ac54b4a17643a2a85217a551d45985c3cd03d26
colspen <spencerlewiscole@gmail.com>
2022-09-28 18:14:12 +0000

    added standard input to handle keyboard commad ctrl+x

--- client.js
+++ client.js
@@ -1,19 +1,32 @@
 const net = require("net");
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541
   });
-
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  
+  console.log("Connecting ...");
+
+  conn.on("connect", () => {
+    console.log("Successfully conected to game server.");
+  });
+
   conn.on("data", (data) => {
     console.log(data);
   });
 
+  conn.on("connect", () => {
+    conn.write("Name: SLC");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 60)
+  });
+
   return conn;
 };
+
+
 
 module.exports = connect;
